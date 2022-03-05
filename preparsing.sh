807c288e695b6f96d85b14e70870a19d5fa94c05
h2th3k <33063404+h2th3k@users.noreply.github.com>
2022-03-05 07:45:39 -0500

    Update preparsing.sh

--- preparsing.sh
+++ preparsing.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-  
-#split every function related info into a file
+
+# please name the output dbgs info from compilation as "raw.log"
+# split every function related info into a file, then use parse.sh to further parse it.
+
 
 func_num=$(grep "ASAN processStaticAllocas" raw.log | wc -l)
 echo "[+] total functions # $func_num"
