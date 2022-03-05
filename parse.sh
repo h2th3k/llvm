166b53996553534f4a68603c00d209f331677fe4
h2th3k <33063404+h2th3k@users.noreply.github.com>
2022-03-05 04:32:02 -0500

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+  
+#split every function related info into a file
 
-#parse the information we will need for statistic
+func_num=$(grep "ASAN processStaticAllocas" raw.log | wc -l)
+echo "[+] total functions # $func_num"
 
-#csplit test.log '/ASAN processStaticAllocas/' '{535}' ---> to split each function per file (535*2 is the total # of ASAN processStaticAllocas)
-#func_name=$(grep function $line | head -n 1 | grep -o -P '(?<=\<).*(?=\>)')
-#buffer_num=$(grep buffer $line | wc -l)
-#poison_size=$(grep h2th3k $line | awk -F ':' '{print $2}' | sed 's/00//g' | sed 's/\$h2th3k\$//g' | wc -w)
+split_num=$(( func_num / 2 ))
+echo "[+] please paste: csplit raw.log '/ASAN processStaticAllocas/' '{$split_num}'"
+
+
+#get the input list for parsing
+
+ls xx* > list.txt
+input="./list.txt"
+
+echo "[+] file # for parsing: $(wc -l list.txt | awk '{print $1}')"
 
 while IFS= read -r line
 do
-       echo "$(grep function $line | head -n 1 | grep -o -P '(?<=\<).*(?=\>)') | $(grep buffer $line | wc -l) | $(grep h2th3k $line | awk -F ':' '{print $2}' | sed 's/00//g' | sed 's/\$h2th3k\$//g' | wc -w)"
+        echo "$(grep function $line | head -n 1 | grep -o -P '(?<=\<).*(?=\>)') | $(grep buffer $line | wc -l) | $(grep h2th3k $line | awk -F ':' '{print $2}' | sed 's/00//g' | sed 's/\$h2th3k\$//g' | wc -w)"
 
-done < "$1"
+done < "$input"
+
